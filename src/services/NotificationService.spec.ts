086b86c41cb90001952574d80321f080e2845ef2
Victor Toschi <victor.toschi@hotmail.com>
2025-03-23 16:20:19 -0300

    create tests

--- src/services/NotificationService.spec.ts
+++ src/services/NotificationService.spec.ts
@@ -1,9 +1,52 @@
-import { NotificationService } from './NotificationService';
+import nodemailer from 'nodemailer';
+import { sendEmail } from './NotificationService';
+
+// Mock nodemailer
+jest.mock('nodemailer');
 
 describe('NotificationService', () => {
-  it('should send notification successfully', async () => {
-    const service = new NotificationService();
-    const result = await service.sendEmail('test@example.com', 'Seu vídeo foi processado!');
+  const mockSendMail = jest.fn().mockResolvedValue({ messageId: 'test-message-id' });
+  
+  const mockCreateTransport = jest.fn().mockReturnValue({
+    sendMail: mockSendMail
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (nodemailer.createTransport as jest.Mock).mockImplementation(mockCreateTransport);
+  });
+
+  it('should send email successfully', async () => {
+    const to = 'test@example.com';
+    const videoName = 'test-video.mp4';
+
+    const result = await sendEmail(to, videoName);
+
     expect(result).toBe(true);
+    expect(nodemailer.createTransport).toHaveBeenCalledWith({
+      host: 'smtp.gmail.com',
+      port: 587,
+      auth: {
+        user: 'usuario_fake',
+        pass: 'senha_fake'
+      }
+    });
+
+    expect(mockSendMail).toHaveBeenCalledWith({
+      from: '"FIAP X Notificações" <no-reply@fiapx.com>',
+      to: 'test@example.com',
+      subject: 'noreply: Atualização sobre seu vídeo',
+      text: `Você está recebendo essa mensagem porque houve um erro no processamento do seu vídeo - test-video.mp4. Verifique a plataforma e tente novamente.`
+    });
+  });
+
+  it('should handle email sending failure', async () => {
+    const error = new Error('Failed to send email');
+    mockSendMail.mockRejectedValueOnce(error);
+
+    const to = 'test@example.com';
+    const videoName = 'test-video.mp4';
+
+    await expect(sendEmail(to, videoName)).rejects.toThrow('Failed to send email');
   });
 });
